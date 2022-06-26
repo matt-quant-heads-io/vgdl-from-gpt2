ed8ede651d7474183d257b310fc18e40980e1011
matt-quant-heads-io <matt.quantheads.io@gmail.com>
2022-06-26 12:57:16 -0400

    Corrected ordering of vgdl_src files

--- src/traits_dict.py
+++ src/traits_dict.py
@@ -1,6 +1,6 @@
 TRAITS_DICT = {
     "zelda": ["9 sprites,", "5 level mappings,", "7 interactions,", "2 termination conditions,"],
-    "boulderdash": ["10 sprites,", "8 level mappings,", "18 interactions,", "2 termination conditions,"],
+    #"boulderdash": ["10 sprites,", "8 level mappings,", "18 interactions,", "2 termination conditions,"],
     "butterflies": ["6 sprites,", "4 level mappings,", "4 interactions,", "2 termination conditions,"],
     "chase": ["6 sprites,", "3 level mappings,", "6 interactions,", "2 termination conditions,"],
     "dodge": ["12 sprites,", "9 level mappings,", "6 interactions,", "2 termination conditions,"],
